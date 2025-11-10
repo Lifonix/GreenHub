2a59a0f71bf643b14e5453ef8088f1ad803be44a
Hyann <contato.hyann@gmail.com>
2025-11-10 18:47:47 -0300

    chore: arrumando package.json e instalando dependencia

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -1,19 +1,24 @@
 import express from "express";
-import cors from "cors";
-import profilesRoutes from "./routes/profiles.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const router = express.Router();
 
-app.use(cors());
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataPath = path.join(__dirname, "../data/profiles.json");
 
-app.use("/api/profissionais", profilesRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API Futuro do Trabalho — backend ativo");
+// GET /profiles
+router.get("/", (req, res) => {
+  fs.readFile(dataPath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("Erro ao ler o JSON:", err);
+      return res.status(500).json({ error: "Erro ao carregar dados" });
+    }
+    const profiles = JSON.parse(data);
+    res.json(profiles);
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend rodando em http://localhost:${PORT}`);
-});
+export default router;
